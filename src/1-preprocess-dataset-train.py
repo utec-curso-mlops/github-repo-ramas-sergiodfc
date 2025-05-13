1e185084afd6a19f2feba99fab37e435acf9eeff
Sergio <sergiodfc@gmail.com>
2025-05-13 18:21:05 -0500

    corrige bug en preprocesamiento datos
    Se corrige bug en limpieza de variables correlacionadas en el script de preprocesamiento de datos 1-preprocess-dataset-train

--- src/1-preprocess-dataset-train.py
+++ src/1-preprocess-dataset-train.py
@@ -3,6 +3,8 @@ import pandas as pd
 import sklearn.metrics as metrics
 import os
 
+#Esta línea va a simular que estamos trabajando en optimizar el código de preprocesamiento de datos
+#Esta 2da línea servirá de apoyo y documentación de los cambios en la optimización del script.
 
 class preprocess_data:
     _output_path = ""
